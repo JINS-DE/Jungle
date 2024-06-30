21bfb5ff1da03ff79669bdda4db9bb38c2fb02df
JS <rlawlstjd044@naver.com>
2024-06-30 20:19:17 +0900

    alonememo app

---
+++ Chapter 4/app.py
@@ -0,0 +1,25 @@
+'''
+flask란?
+파이썬으로 쓰인 웹 프레임워크로, 서버를 구동하는 데 필요한 여러 기능들을 제공합니다.
+'''
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/mypage')
+def mypage():  
+   return 'This is My Page!'
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
+
+@app.route('/test', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
